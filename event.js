e98c5395b8a7ed2e9d2ea4e3dec08da5b07c50e6
Constronov <constronov3@googlemail.com>
2023-05-02 18:10:44 +0200

    + event alien-adventures

---
+++ event.js
@@ -0,0 +1,11 @@
+(async () => {
+    while (1) {
+      timer = document.querySelector("#all-slots > div > div.all-slots-in > div:nth-child(1) > div > div.one-alien-slot > div.descr-slot > div > span");
+      if(timer.innerText == 'SEND TO ADVENTURE') {
+        buttonMint = document.querySelector("#all-slots > div > div.register-alien > div");
+        buttonMint.click();
+      }
+      await new Promise((res) => setTimeout(res, 1000));
+    }
+  })();
+  
