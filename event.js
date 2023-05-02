afd9b5c82f475550887d5130aa1972ef633cac70
Constronov <constronov3@googlemail.com>
2023-05-02 18:16:05 +0200

    + null error

--- event.js
+++ event.js
@@ -1,7 +1,7 @@
 (async () => {
     while (1) {
       timer = document.querySelector("#all-slots > div > div.all-slots-in > div:nth-child(1) > div > div.one-alien-slot > div.descr-slot > div > span");
-      if(timer.innerText == 'SEND TO ADVENTURE') {
+      if(timer != null && timer.innerText == 'SEND TO ADVENTURE') {
         buttonMint = document.querySelector("#all-slots > div > div.register-alien > div");
         buttonMint.click();
       }
