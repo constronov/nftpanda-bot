d37d501e38382fc6573ff7b7b90df9cab0c20fc8
Constronov <constronov3@googlemail.com>
2023-08-04 17:02:28 +0200

    + fix for event

--- event.js
+++ event.js
@@ -4,7 +4,13 @@
       if(timer != null && timer.innerText == 'SEND TO ADVENTURE') {
         buttonMint = document.querySelector("#all-slots > div > div.register-alien > div");
         buttonMint.click();
-		await new Promise((res) => setTimeout(res, 1000000));
+        await new Promise((res) => setTimeout(res, 1000));
+
+        buttonMintWithoutPotion = document.querySelector("#root > div > div.modal-to-adv.reward-window.open.anim.alien-rew > div.modal-to-adv-body > div.modal-to-adv-body-in.multi-rew > div > div.mdl-gtav-mid > div > div:nth-child(1) > div.one-adv-bl-descr > div.one-adv-bl-bt > div");
+        if (buttonMintWithoutPotion != null) {
+          buttonMintWithoutPotion.click();
+          await new Promise((res) => setTimeout(res, 1000000));
+        }
       }
       await new Promise((res) => setTimeout(res, 1000));
     }
